492ec137124343917e1508e6f907a76352509b99
mcushing <matt@cush.me>
2021-01-04 07:27:28 -0600

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+import ifitlink.const as const
+import ifitlink.get_googleaccount as get_googleaccount
+import ifitlink.get_ifitaccount as get_ifitaccount
+import ifitlink.google_datasources as google_datasources
+import ifitlink.ifitsync as ifitsync
